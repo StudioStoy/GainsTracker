cb16b4cf8d1ebaa432a24ac61a961cfe78a438fa
Stijuh <stijevoh@gmail.com>
2023-04-01 20:52:42 +0200

    Improved abstraction for Workout and Measurement derived classes

--- GainsTrackerAPI/Gains/Models/Measurements/Measurement.cs
+++ GainsTrackerAPI/Gains/Models/Measurements/Measurement.cs
@@ -1,9 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 using GainsTrackerAPI.Gains.Models.Measurements.Units;
 
 namespace GainsTrackerAPI.Gains.Models.Measurements;
 
 [Table("measurement")]
+[JsonDerivedType(typeof(WeightMeasurement))]
+[JsonDerivedType(typeof(RunningMeasurement))]
+[JsonDerivedType(typeof(SimpleEnduranceMeasurement))]
+[JsonDerivedType(typeof(SimpleRepMeasurement))]
 public abstract class Measurement
 {
     public DateTime TimeOfRecord { get; } = DateTime.Now;
--- GainsTrackerAPI/Gains/Models/Workout.cs
+++ GainsTrackerAPI/Gains/Models/Workout.cs
@@ -1,13 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 using GainsTrackerAPI.Gains.Models.Measurements;
 
 namespace GainsTrackerAPI.Gains.Models;
 
 [Table("workout")]
+[JsonDerivedType(typeof(EnduranceWorkout))]
+[JsonDerivedType(typeof(PureRepWorkout))]
+[JsonDerivedType(typeof(RunningWorkout))]
+[JsonDerivedType(typeof(WeightWorkout))]
 public abstract class Workout
 {
-    public WorkoutType Type { get; set; }
     public List<Measurement> Measurements { get; set; } = new();
+
+    public WorkoutType Type { get; set; }
+
+    protected abstract void CheckPersonalBest<T>(Measurement newMeasurement, T oldPersonalBest);
 
     #region Relations
 
@@ -20,19 +28,51 @@ public abstract class Workout
 public class EnduranceWorkout : Workout
 {
     public double PersonalBest { get; set; }
+
+    protected override void CheckPersonalBest<T>(Measurement newMeasurement, T oldPersonalBest)
+    {
+        if (newMeasurement.GetType() != typeof(SimpleEnduranceMeasurement)) return;
+        double newReps = (newMeasurement as SimpleEnduranceMeasurement)!.Time;
+
+        if (newReps > PersonalBest)
+            PersonalBest = newReps;
+    }
 }
 
 public class PureRepWorkout : Workout
 {
     public int PersonalBest { get; set; }
+
+    protected override void CheckPersonalBest<T>(Measurement newMeasurement, T oldPersonalBest)
+    {
+        if (newMeasurement.GetType() != typeof(SimpleRepMeasurement)) return;
+        int newReps = (newMeasurement as SimpleRepMeasurement)!.Reps;
+
+        if (newReps > PersonalBest)
+            PersonalBest = newReps;
+    }
 }
 
 public class RunningWorkout : Workout
 {
     public double PersonalBest { get; set; }
+
+    protected override void CheckPersonalBest<T>(Measurement newMeasurement, T oldPersonalBest)
+    {
+        throw new NotImplementedException();
+    }
 }
 
 public class WeightWorkout : Workout
 {
     public double PersonalBest { get; set; }
+
+    protected override void CheckPersonalBest<T>(Measurement newMeasurement, T oldPersonalBest)
+    {
+        if (newMeasurement.GetType() != typeof(WeightMeasurement)) return;
+        double newWeight = (newMeasurement as WeightMeasurement)!.Weight;
+
+        if (newWeight > PersonalBest)
+            PersonalBest = newWeight;
+    }
 }
