b39222348608d1ce8c98fa1489dd5e36652989bd
StijeHardeman <stije.it@gmail.com>
2024-09-26 23:12:16 +0200

    Updated render-build.sh version to .NET 8 download.

--- GainsTracker.ClientWebAssembly/render-build.sh
+++ GainsTracker.ClientWebAssembly/render-build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # Download
-wget https://download.visualstudio.microsoft.com/download/pr/ff8c660f-ffa9-4814-ac2d-4089e6ec4eb5/dc806d344844f1d58d8015d105e85c65/dotnet-sdk-7.0.403-linux-x64.tar.gz;
+wget https://download.visualstudio.microsoft.com/download/pr/1ebffeb0-f090-4001-9f13-69f112936a70/5dbc249b375cca13ec4d97d48ea93b28/dotnet-sdk-8.0.402-linux-x64.tar.gz
 
 # Extract
 mkdir -p $XDG_CACHE_HOME/dotnet && tar zxf dotnet-sdk-7.0.403-linux-x64.tar.gz -C $XDG_CACHE_HOME/dotnet;
