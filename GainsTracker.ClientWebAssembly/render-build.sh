a13ebf76e34f3d64d4a088863bd07e9caf32cdd8
StijeHardeman <stije.it@gmail.com>
2024-09-26 23:13:39 +0200

    Updated extraction script to use .NET 8

--- GainsTracker.ClientWebAssembly/render-build.sh
+++ GainsTracker.ClientWebAssembly/render-build.sh
@@ -6,7 +6,7 @@ set -o errexit
 wget https://download.visualstudio.microsoft.com/download/pr/1ebffeb0-f090-4001-9f13-69f112936a70/5dbc249b375cca13ec4d97d48ea93b28/dotnet-sdk-8.0.402-linux-x64.tar.gz
 
 # Extract
-mkdir -p $XDG_CACHE_HOME/dotnet && tar zxf dotnet-sdk-7.0.403-linux-x64.tar.gz -C $XDG_CACHE_HOME/dotnet;
+mkdir -p $XDG_CACHE_HOME/dotnet && tar zxf dotnet-sdk-8.0.402-linux-x64.tar.gz -C $XDG_CACHE_HOME/dotnet;
 
 # Set vars
 export DOTNET_ROOT=$XDG_CACHE_HOME/dotnet;
