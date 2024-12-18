82e8a22fa3542cab99617be6012dcdcba054199f
StijeHardeman <stije.it@gmail.com>
2024-12-18 22:40:10 +0100

    Added GtBase component

---
+++ GainsTracker.UI/Components/Shared/GtBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GainsTracker.UI.Components.Shared;
+
+public abstract class GtBase : ComponentBase
+{
+    /// <summary>
+    /// Additional CSS classes for the component.
+    /// </summary>
+    [Parameter] public string? Class { get; set; }
+}
