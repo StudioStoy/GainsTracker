1169d4132f8bfd722b90c868043050fa7491c6ec
StijeHardeman <stije.it@gmail.com>
2024-12-22 00:10:32 +0100

    Added GtGroup component for easier validation functionality in GtBase and implementing components.

--- GainsTracker.UI/Components/Shared/GtBase.cs
+++ GainsTracker.UI/Components/Shared/GtBase.cs
@@ -8,4 +8,23 @@ public abstract class GtBase : ComponentBase
     /// Additional CSS classes for the component.
     /// </summary>
     [Parameter] public string? Class { get; set; }
+
+    /// <summary>
+    /// The group which this component is registered to, if in group.
+    /// </summary>
+    /// <remarks>Optional.</remarks>
+    [CascadingParameter] private GtGroup.GtGroup? ParentGtGroup { get; set; }
+
+    protected override void OnInitialized()
+    {
+        ParentGtGroup?.RegisterChild(this);
+        base.OnInitialized();
+    }
+
+    /// <summary>
+    /// Validates the input of the component if required. 
+    /// </summary>
+    /// <remarks>Defaults to true if it's a component without inputs.</remarks>
+    /// <returns></returns>
+    public virtual bool Validate() => true;
 }
