e4b2b026914722c9ac685d2e985c02d23e96e651
StijeHardeman <stije.it@gmail.com>
2025-03-29 11:54:11 +0100

    Updated GtModal to be closable using Esc key.

--- GainsTracker.UI.Elements/Modal/ModalService.cs
+++ GainsTracker.UI.Elements/Modal/ModalService.cs
@@ -10,15 +10,16 @@ public class ModalService
 
     public void Open(Type type)
     {
+        // If not already in the stack, add the modal to the top of the stack.
         if (_modalStack.Contains(type)) return;
-        Console.WriteLine($"Opening type {type}");
         _modalStack.Add(type);
         NotifyStateChanged();
     }
     
     public void CloseLast()
     {
-        // Remove the last opened modal on the stack.
+        // Remove the most recently opened modal of the stack.
+        if (_modalStack.Count == 0) return;
         _modalStack.RemoveAt(_modalStack.Count-1);
         NotifyStateChanged();
     }
